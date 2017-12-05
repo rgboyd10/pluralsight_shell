8f9b8e8c12ad7b509883f3b40231d243a5f90af9
rgboyd10 <rgboyd10@gmail.com>
2017-12-05 13:40:11 -0500

    -

--- conditional.sh
+++ conditional.sh
@@ -2,6 +2,10 @@
 
 if [[ $# -eq 0 ]] ; then 
     echo -e "\nWait.... you didn't enter any arguments\n"
+    echo -e "Usage: $(basename $0) <arg1> <arg2> ... <argn>\n"
 else
     echo -e "\nYou typed the following arguments: $@\n"
+    exit 1
 fi
+
+echo "We'll only see this if an argument was specified."
